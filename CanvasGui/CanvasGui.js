b901af89d0d781481e71d0ab846d515782258294
Mecoolnotcool <106288499+Mecoolnotcool@users.noreply.github.com>
2025-08-11 23:02:18 -0500

    finished with the basics

--- CanvasGui/CanvasGui.js
+++ CanvasGui/CanvasGui.js
@@ -1,13 +1,58 @@
-let canvas = document.getElementById('canvas');
+let canvas = document.getElementById('Canvas');
 let ctx = canvas.getContext('2d');
 
-const ButtonCreator = {
-    rect : canvas.getBoundingClientRect(),
+const GuiCreator = {
+    frame:  class {
+        constructor(x,y,width,height,clickable,name,color){
+            this.height = height;
+            this.width = width;
+            this.x = x;
+            this.y = y;
+            this.clickable = clickable;
+            this.name = name;
+            this.children = {};
+            this.offsets;
+            this.color = color;
+        }
+        addChild(NewChild,NewId){
+            if(NewChild != undefined || NewChild != null) {
+                NewChild.parent = this;
+                this.children[NewId] = NewChild;
+            } else{
+                console.error('failed to add child to object')
+                return
+            }  
+        }
+        drawChildren(){
+            Object.keys(this.children).forEach(key => {
+                    let toDrawChild = this.children[key]
+                    if(typeof toDrawChild === 'object'){
+                      GuiCreator.drawBox(toDrawChild.x,toDrawChild.y,toDrawChild.color,toDrawChild.width,toDrawChild.height)
+                    }
+            })
+        }
+    },
 
+    buttonBase: class {
+        constructor(x,y,width,height,clickable,name,color){
+            this.height = height;
+            this.width = width;
+            this.x = x;
+            this.y = y;
+            this.clickable = clickable;
+            this.name = name;
+            this.children = {};
+            this.parent = null;
+            this.color = color;
+        }
+    },
+
+    rect : canvas.getBoundingClientRect(),
     buttonList : {},
-    createButton:function(id,x,y,color,width,height){
-        let NewButton = new  this.buttonBox(x,y,id,color,width,height);
-        this.buttonList[id] = NewButton
+
+    //methods 
+    addObject:function(obj,name){
+       this.buttonList[name] = obj;
     },
     handleClick:function(Ex,Ey) {
        let mouseX = Ex - this.rect.left;
@@ -17,14 +62,39 @@ const ButtonCreator = {
        if (Clicked == true){
          console.log('clicked a button')
        }
+
+       let ChildClicked = this.checkChildCollison(mouseX,mouseY)
+       if (ChildClicked == true){
+         console.log('clicked a child button')
+       }
     },
     checkCollision:function(mouseX,mouseY) {
         let collided = false;
         Object.keys(this.buttonList).forEach(key =>{
-            let button = this.buttonList[key];
-            if (mouseX >= button.x && mouseX <= button.width + button.x && mouseY >= button.y && mouseY <= button.height + button.y){
-                collided = true;
+            let UiObj = this.buttonList[key];
+            if (mouseX >= UiObj.x && mouseX <= UiObj.width + UiObj.x && mouseY >= UiObj.y && mouseY <= UiObj.height + UiObj.y){
+                if(UiObj.clickable){
+                    collided = true;
+                }
             }
+        })
+        return collided
+    },
+    checkChildCollison:function(mouseX,mouseY){
+        let collided = false;
+        Object.keys(this.buttonList).forEach(key =>{
+            let UiObj = this.buttonList[key];
+            if(UiObj.children){
+                Object.keys(UiObj.children).forEach(key =>{
+                    let child = UiObj.children[key]
+                    if(mouseX >= child.x && mouseX <= child.width + child.x && mouseY >= child.y && mouseY <= child.height + child.y){
+                        if(child.clickable){
+                            collided = true
+                        }
+                    }
+                });
+            }
+            
         })
         return collided
     },
@@ -33,37 +103,83 @@ const ButtonCreator = {
         ctx.fillRect(x,y,width,height);
     },
     drawloop: function(){
-        ctx.clearRect(0,0,1000,1000)
+        ctx.clearRect(0,0,1000,1000);
+        this.drawText("are you sure u wanna buy this bro");
         Object.keys(this.buttonList).forEach(key => {
             let toDraw = this.buttonList[key]; 
             this.drawBox(toDraw.x,toDraw.y,toDraw.color,toDraw.width,toDraw.height);
+            toDraw.drawChildren()
         });
         requestAnimationFrame(this.drawloop.bind(this));
-    },   
-    buttonBox: class{
-        constructor(x,y,id,color,width,height){
-            this.x = x;
-            this.y = y;
-            this.id = id;
-            this.color  = color;
-            this.width = width;
-            this.height = height;
+    },  
+    //this automatically creates offsets for your ui elements
+    createOffsets(parent){
+        let finalOffsets = {};
+        if(parent.children){
+            Object.keys(parent.children).forEach(key =>{
+                let child = parent.children[key];
+                    finalOffsets[child.name] = {
+                        "xOffset":child.x - parent.x,
+                        "yOffset":child.y - parent.y,
+                    };
+            });
         }
+        return finalOffsets;
+    },
+    setInitalPosition:function(main,x,y){
+        main.x = x;
+        main.y = y;
+        if(main.children && main.offsets){
+            Object.keys(main.children).forEach(key =>{
+                let child = main.children[key];
+                if(main.offsets.hasOwnProperty(child.name)){
+                    child.x = main.x + main.offsets[child.name].xOffset;
+                    child.y = main.y + main.offsets[child.name].yOffset;
+                }
+            });
+        }
+    },
+    drawText:function(txt){
+        ctx.fillStyle = "#009578"
+        ctx.fillText(txt,100,100);
     }
+    
         
 }
 
 
-ButtonCreator.createButton("button1",0,0,"red",100,100);
-ButtonCreator.createButton("button2",200,200,"gray",100,100);
-ButtonCreator.createButton("button3",300,0,"teal",100,100);
-ButtonCreator.createButton("button4",500,0,"purple",100,100);
-ButtonCreator.createButton("button5",0,500,"violet",100,100);
+let buttons = {}
+
+//create a basic re-usable yes or no button
+const YesOrNoButton = {
+    createThisButton:function(name,UiX,UiY){
+        let frame = new GuiCreator.frame(0,0,200,200,false,"FrameBackground",'blue');
+        let yesButton = new GuiCreator.buttonBase(30,150,40,40,true,"yesButton",'green');
+        let noButton = new GuiCreator.buttonBase(130,150,40,40,true,"noButton",'red');
+
+        frame.addChild(yesButton,yesButton.name);
+        frame.addChild(noButton,noButton.name);
+        frame.offsets = GuiCreator.createOffsets(frame);
+
+        GuiCreator.addObject(frame,name);
+        GuiCreator.setInitalPosition(frame,UiX,UiY);
+        
+    }
+}
+//creating it
+YesOrNoButton.createThisButton("ConfirmBuyButton2",300,300);
+
+//clickable frame
+let frame = new GuiCreator.frame(0,500,200,200,true,"FrameBackground",'blue')
+GuiCreator.addObject(frame,'clickableFrame');
+
+
+GuiCreator.drawText()
 
 
 //setup the game loop and click detection
-requestAnimationFrame(() => ButtonCreator.drawloop());
-
+requestAnimationFrame(() => GuiCreator.drawloop());
 canvas.addEventListener('click', (event) => {
-    ButtonCreator.handleClick(event.x,event.y)
+    GuiCreator.handleClick(event.x,event.y)
 })
+
