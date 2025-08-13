3a7360604984feeaf3e27ac033a902bf19031df5
Mecoolnotcool <106288499+Mecoolnotcool@users.noreply.github.com>
2025-08-12 23:37:39 -0500

    Added an example of how to use this

---
+++ CanvasGui/example.js
@@ -0,0 +1,49 @@
+import { GuiCreator, canvas } from './CanvasGui.js';
+
+//create a basic re-usable yes or no button
+const YesOrNoButton = {
+    createThisButton:function(name,UiX,UiY){
+        let frame = new GuiCreator.frame(0,0,200,200,false,"FrameBackground",'blue');
+        let yesButton = new GuiCreator.buttonBase(30,150,40,40,true,"yesButton",'green');
+        let noButton = new GuiCreator.buttonBase(130,150,40,40,true,"noButton",'red');
+
+        frame.addChild(yesButton,yesButton.name);
+        frame.addChild(noButton,noButton.name);
+        frame.offsets = GuiCreator.createOffsets(frame);
+
+        GuiCreator.createButtonEvent('yesButton','yesButtonClicked');
+        GuiCreator.createButtonEvent('noButton','noButtonClicked');
+
+        GuiCreator.addObject(frame,name);
+        GuiCreator.setInitalPosition(frame,UiX,UiY);
+        
+    }
+}
+//creating it
+YesOrNoButton.createThisButton("ConfirmBuyButton2",300,300);
+
+//clickable frame
+let frame = new GuiCreator.frame(0,500,200,200,true,"FrameBackground",'blue')
+GuiCreator.addObject(frame,'clickableFrame');
+
+
+GuiCreator.drawText()
+
+console.log(GuiCreator.clickMethods)
+
+
+//setup the game loop and click detection
+requestAnimationFrame(() => GuiCreator.drawloop());
+canvas.addEventListener('click', (event) => {
+    GuiCreator.handleClick(event.x,event.y)
+})
+
+//button event listeners
+document.addEventListener('yesButtonClicked', (event) => {
+    console.log(event.detail.ButtonClicked);
+    //add logic here (the console.log can be removed)
+});
+document.addEventListener('noButtonClicked', (event) => {
+    console.log(event.detail.ButtonClicked);
+    //add logic here (the console.log can be removed)
+});
